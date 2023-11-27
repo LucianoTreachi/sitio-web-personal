fcd4ea166a9c7b9218c6eb2e413f9def809535fe
lucianotreachi <lucianotreachi@outlook.com>
2023-11-26 22:21:34 -0300

    Modificar la forma en como aparece el menú dropdown de idiomas

--- js/indexLanguages.js
+++ js/indexLanguages.js
@@ -23,12 +23,12 @@ const dropdownMenu = document.querySelector(".dropdown-menu");
 // Close the menu when clicked outside
 document.addEventListener("click", (event) => {
   if (!dropdownMenu.contains(event.target) && !dropdownButton.contains(event.target)) {
-    dropdownMenu.style.display = "none";
+    dropdownMenu.classList.remove("active");
   }
 });
 
 dropdownButton.addEventListener("click", () => {
-  dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+  dropdownMenu.classList.toggle("active");
 });
 
 // Get the language stored in localStorage (if it exists)
@@ -43,7 +43,7 @@ languageLinks.forEach(link => {
     event.preventDefault();
     const selectedLang = event.target.dataset.lang.toLowerCase(); // Get the language from the data-lang attribute
     changeLanguage(selectedLang);
-    dropdownMenu.style.display = "none";
+    dropdownMenu.classList.remove("active");
     dropdownButton.innerHTML = event.target.innerHTML + '<svg class="chevron-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 15.0006L7.75732 10.758L9.17154 9.34375L12 12.1722L14.8284 9.34375L16.2426 10.758L12 15.0006Z"></path></svg>';
   });
 });
