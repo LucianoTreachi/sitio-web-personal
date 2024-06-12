4c531b52af31637d76e05222db821becb45d7fc6
lucianotreachi <lucianotreachi@outlook.com>
2024-06-12 08:42:37 -0300

    Modificar información

--- js/original/projectsLanguages.js
+++ js/original/projectsLanguages.js
@@ -18,67 +18,54 @@ const projectsTranslations = {
     /* Project 1 */
     project1Title: "Curriculum Vitae Web",
     project1Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un Curriculum Vitae Web.",
-    project1Paragraph2: "<b>Tecnologías utilizadas:</b> HTML, CSS, JavaScript, Particles JS.",
 
     /* Project 2 */
     project2Title: "Landing Page para Emmanuel Mansilla",
     project2Paragraph1: "<b>Descripción del proyecto:</b> landing page para Emmanuel Mansilla, propietario de una Consultora de Negocios.",
-    project2Paragraph2: "<b>Tecnologías utilizadas:</b> Vite, React, CSS Modules, Cloudinary, Email JS.",
 
     /* Project 3 */
     project3Title: "Contador de Texto",
     project3Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé una herramienta online que permite obtener rápidamente el número de caracteres, palabras, oraciones y párrafos de un texto.",
-    project3Paragraph2: "<b>Tecnologías utilizadas:</b> HTML, CSS, JavaScript.",
 
     /* Project 4 */
     project4Title: "Conversor de Letras",
     project4Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un conversor de letras que transforma texto plano en caracteres UNICODE en tiempo real, permitiendo a los usuarios copiar y pegar el texto convertido en diversas redes sociales.",
-    project4Paragraph2: "<b>Tecnologías utilizadas:</b> HTML, CSS, JavaScript.",
 
     /* Project 5 */
     project5Title: "Academia Online",
     project5Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un sitio web para una academia que vende cursos online.",
-    project5Paragraph2: "<b>Tecnologías utilizadas:</b> HTML, CSS, Sass, Bootstrap, JavaScript, Particles JS, WOW JS, Swiper JS.",
 
     /* Project 6 */
     project6Title: "Academia Online",
     project6Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un sitio web para una academia que vende cursos online.",
-    project6Paragraph2: "<b>Tecnologías utilizadas:</b> HTML, CSS, JavaScript, Particles JS, WOW JS, Swiper JS.",
 
     /* Project 7 */
     project7Title: "Luciano Guitar Beats",
     project7Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un sitio web de venta de composiciones musicales.",
-    project7Paragraph2: "<b>Tecnologías utilizadas:</b> HTML, CSS, JavaScript, Swiper JS, Email JS.",
 
     /* Project 8 */
     project8Title: "Sitio Web para la provincia de Córdoba",
     project8Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un sitio web para la provincia de Córdoba, ubicada en Argentina. El sitio muestra 80 lugares turísticos para visitar.",
-    project8Paragraph2: "<b>Tecnologías utilizadas:</b> HTML, CSS, JavaScript, Cloudinary, Email JS.",
 
     /* Project 9 */
     project9Title: "E-commerce Dedicado a la Venta De Guitarras",
     project9Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un pequeño e-commerce que vende guitarras.",
-    project9Paragraph2: "<b>Tecnologías utilizadas:</b> HTML, CSS, JavaScript.",
 
     /* Project 10 */
     project10Title: "E-commerce Dedicado a la Venta De Libros",
     project10Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un pequeño e-commerce que vende libros de Desarrollo Frontend.",
-    project10Paragraph2: "<b>Tecnologías utilizadas:</b> Vite, React, CSS modules, Cloudinary, Firebase.",
 
     /* Project 11 */
     project11Title: "Blog sobre estrategias UX/UI",
     project11Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un Blog sobre consejos, técnicas y estrategias UX/UI para un diseño atractivo, intuitivo y funcional.",
-    project11Paragraph2: "<b>Tecnologías utilizadas:</b> Vite, React, CSS modules, Cloudinary.",
 
     /* Project 12 */
     project12Title: "Cursos Online de Programación",
     project12Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un sitio web que reúne los mejores cursos online de programación, clasificados por Frontend, Backend y Aplicaciones Móviles.",
-    project12Paragraph2: "<b>Tecnologías utilizadas:</b> Next.js 14, CSS Modules, Cloudinary",
 
     /* Project 13 */
     project13Title: "Optimización Web",
     project13Paragraph1: "<b>Descripción del proyecto:</b> diseñé y desarrollé un sitio web que ofrece una lista de verificaciones para optimizar el Desarrollo Web y lograr una presencia en línea impecable.",
-    project13Paragraph2: "<b>Tecnologías utilizadas:</b> HTML, CSS, JavaScript",
   },
 
   English: {
@@ -100,67 +87,54 @@ const projectsTranslations = {
     /* Project 1 */
     project1Title: "Web Curriculum Vitae",
     project1Paragraph1: "<b>Project Description:</b> I designed and developed a Web Curriculum Vitae.",
-    project1Paragraph2: "<b>Technologies Used:</b> HTML, CSS, JavaScript, Particles JS.",
 
     /* Project 2 */
     project2Title: "Landing Page for Emmanuel Mansilla",
     project2Paragraph1: "<b>Project Description:</b> landing page for Emmanuel Mansilla, owner of a Business Consulting Firm.",
-    project2Paragraph2: "<b>Technologies Used:</b> Vite, React, CSS Modules, Cloudinary, Email JS.",
 
     /* Project 3 */
     project3Title: "Text Counter",
     project3Paragraph1: "<b>Project Description:</b> I designed and developed an online tool that allows quickly obtaining the number of characters, words, sentences, and paragraphs in a text.",
-    project3Paragraph2: "<b>Technologies Used:</b> HTML, CSS, JavaScript.",
 
     /* Project 4 */
     project4Title: "Letter Converter",
     project4Paragraph1: "<b>Project Description:</b> I designed and developed a letter converter that transforms plain text into UNICODE characters in real-time, allowing users to copy and paste the converted text on various social media platforms.",
-    project4Paragraph2: "<b>Technologies Used:</b> HTML, CSS, JavaScript.",
 
     /* Project 5 */
     project5Title: "Online Academy",
     project5Paragraph1: "<b>Project Description:</b> I designed and developed a website for an online academy that offers courses.",
-    project5Paragraph2: "<b>Technologies Used:</b> HTML, CSS, Sass, Bootstrap, JavaScript, Particles JS, WOW JS, Swiper JS.",
 
     /* Project 6 */
     project6Title: "Online Academy",
     project6Paragraph1: "<b>Project Description:</b> I designed and developed a website for an online academy that offers courses.",
-    project6Paragraph2: "<b>Technologies Used:</b> HTML, CSS, JavaScript, Particles JS, WOW JS, Swiper JS.",
 
     /* Project 7 */
     project7Title: "Luciano Guitar Beats",
     project7Paragraph1: "<b>Project Description:</b> I designed and developed a website for the sale of musical compositions.",
-    project7Paragraph2: "<b>Technologies Used:</b> HTML, CSS, JavaScript, Swiper JS, Email JS.",
 
     /* Project 8 */
     project8Title: "Website for the province of Córdoba",
     project8Paragraph1: "<b>Project Description:</b> I designed and developed a website for the province of Córdoba, located in Argentina. The site showcases 80 tourist destinations to visit.",
-    project8Paragraph2: "<b>Technologies Used:</b> HTML, CSS, JavaScript, Cloudinary, Email JS.",
 
     /* Project 9 */
     project9Title: "Dedicated Guitar Sales E-commerce",
     project9Paragraph1: "<b>Project Description:</b> I designed and developed a small e-commerce website that sells guitars.",
-    project9Paragraph2: "<b>Technologies Used:</b> HTML, CSS, JavaScript.",
 
     /* Project 10 */
     project10Title: "Dedicated Book Sales E-commerce",
     project10Paragraph1: "<b>Project Description:</b> I designed and developed a small e-commerce website that sells Frontend Development books.",
-    project10Paragraph2: "<b>Technologies Used:</b> Vite, React, CSS modules, Cloudinary, Firebase.",
 
     /* Project 11 */
     project11Title: "Blog about UX/UI strategies",
     project11Paragraph1: "<b>Project Description:</b> I designed and developed a blog about tips, techniques, and UX/UI strategies for an attractive, intuitive, and functional design.",
-    project11Paragraph2: "<b>Technologies Used:</b> Vite, React, CSS modules, Cloudinary.",
 
     /* Project 12 */
     project12Title: "Online Programming Courses",
     project12Paragraph1: "<b>Project Description:</b> I designed and developed a website that brings together the best online programming courses, classified by Frontend, Backend, and Mobile Applications.",
-    project12Paragraph2: "<b>Technologies Used:</b> Next.js 14, CSS Modules, Cloudinary",
 
     /* Project 13 */
     project13Title: "Web Optimization",
     project13Paragraph1: "<b>Project Description:</b> I designed and developed a website that offers a checklist to optimize Web Development and achieve impeccable online presence. ",
-    project13Paragraph2: "<b>Technologies Used:</b> HTML, CSS, JavaScript",
   },
 
   Portugues: {
@@ -182,67 +156,54 @@ const projectsTranslations = {
     /* Project 1 */
     project1Title: "Currículo Online",
     project1Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um Curriculum Vitae Web.",
-    project1Paragraph2: "<b>Tecnologias utilizadas:</b> HTML, CSS, JavaScript, Particles JS.",
 
     /* Project 2 */
     project2Title: "Página de Destino para Emmanuel Mansilla",
     project2Paragraph1: "<b>Descrição do projeto:</b> página de destino para Emmanuel Mansilla, proprietário de uma Consultoria de Negócios.",
-    project2Paragraph2: "<b>Tecnologias utilizadas:</b> Vite, React, CSS Modules, Cloudinary, Email JS.",
 
     /* Project 3 */
     project3Title: "Contador de Texto",
     project3Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi uma ferramenta online que permite obter rapidamente o número de caracteres, palavras, frases e parágrafos de um texto.",
-    project3Paragraph2: "<b>Tecnologias utilizadas:</b> HTML, CSS, JavaScript.",
 
     /* Project 4 */
     project4Title: "Conversor de Letras",
     project4Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um conversor de letras que transforma texto simples em caracteres UNICODE em tempo real, permitindo que os usuários copiem e colem o texto convertido em diversas redes sociais.",
-    project4Paragraph2: "<b>Tecnologias utilizadas:</b> HTML, CSS, JavaScript.",
 
     /* Project 5 */
     project5Title: "Academia Online",
     project5Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um site para uma academia que vende cursos online.",
-    project5Paragraph2: "<b>Tecnologias utilizadas:</b> HTML, CSS, Sass, Bootstrap, JavaScript, Particles JS, WOW JS, Swiper JS.",
 
     /* Project 6 */
     project6Title: "Academia Online",
     project6Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um site para uma academia que vende cursos online.",
-    project6Paragraph2: "<b>Tecnologias utilizadas:</b> HTML, CSS, JavaScript, Particles JS, WOW JS, Swiper JS.",
 
     /* Project 7 */
     project7Title: "Luciano Guitar Beats",
     project7Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um site de venda de composições musicais.",
-    project7Paragraph2: "<b>Tecnologias utilizadas:</b> HTML, CSS, JavaScript, Swiper JS, Email JS.",
 
     /* Project 8 */
     project8Title: "Site para a província de Córdoba",
     project8Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um site para a província de Córdoba, localizada na Argentina. O site apresenta 80 locais turísticos para visitar.",
-    project8Paragraph2: "<b>Tecnologias utilizadas:</b> HTML, CSS, JavaScript, Cloudinary, Email JS.",
 
     /* Project 9 */
     project9Title: "E-commerce dedicado à venda de guitarras",
     project9Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um pequeno e-commerce que vende guitarras.",
-    project9Paragraph2: "<b>Tecnologias utilizadas:</b> HTML, CSS, JavaScript.",
 
     /* Project 10 */
     project10Title: "E-commerce dedicado à venda de livros",
     project10Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um pequeno e-commerce que vende livros de Desenvolvimento Frontend.",
-    project10Paragraph2: "<b>Tecnologias utilizadas:</b> Vite, React, CSS modules, Cloudinary, Firebase.",
 
     /* Project 11 */
     project11Title: "Blog sobre estratégias de UX/UI",
     project11Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um blog sobre dicas, técnicas e estratégias de UX/UI para um design atraente, intuitivo e funcional.",
-    project11Paragraph2: "<b>Tecnologias utilizadas:</b> Vite, React, CSS modules, Cloudinary.",
 
     /* Project 12 */
     project12Title: "Cursos Online de Programação",
     project12Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um site que reúne os melhores cursos online de programação, classificados por Frontend, Backend e Aplicações Móveis.",
-    project12Paragraph2: "<b>Tecnologias utilizadas:</b> Next.js 14, CSS Modules, Cloudinary",
 
     /* Project 13 */
     project13Title: "Otimização Web",
     project13Paragraph1: "<b>Descrição do projeto:</b> criei e desenvolvi um site que oferece uma lista de verificação para otimizar o Desenvolvimento Web e alcançar uma presença online impecável.",
-    project13Paragraph2: "<b>Tecnologias utilizadas:</b> HTML, CSS, JavaScript",
   }
 };
 
