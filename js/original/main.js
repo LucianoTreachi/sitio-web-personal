50d7a32668daa63cf7849fc3aaa49559b6ec5df9
lucianotreachi <lucianotreachi@outlook.com>
2025-08-02 15:06:02 -0300

    Restablecer código JS previo al uso del atributo inert y configurar visibility en el menú de navegación

--- js/original/main.js
+++ js/original/main.js
@@ -10,9 +10,6 @@ const overlay = document.querySelector(".overlay");
 const navLinks = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll('section');
 
-// Media query (57rem = 912px)
-const mediaQuery = window.matchMedia("(max-width: 57rem)");
-
 // SKIP TO MAIN CONTENT
 skipLink.addEventListener("click", (event) => {
   event.preventDefault();
@@ -33,56 +30,35 @@ skipLink.addEventListener("keydown", (event) => {
 });
 
 // NAVBAR
-function disableNavbar() {
-  navbar.setAttribute("aria-hidden", "true");
-  navbar.setAttribute("inert", "");
-}
-
-function enableNavbar() {
-  navbar.setAttribute("aria-hidden", "false");
-  navbar.removeAttribute("inert");
-}
-
-function updateNavbarVisibility() {
-  if (mediaQuery.matches) {
-    if (!navbar.classList.contains("active")) {
-      disableNavbar();
-    }
-  } else {
-    enableNavbar();
-  }
-}
-
 function openNavbar() {
   navbar.classList.add("active");
   overlay.classList.add("active");
-  enableNavbar();
   openMenuButton.setAttribute("aria-expanded", "true");
   openMenuButton.setAttribute("aria-hidden", "true");
   closeMenuButton.focus();
 }
 
-function closeNavbar(shouldFocus = false) {
+function closeNavbarWithFocus() {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
   openMenuButton.setAttribute("aria-expanded", "false");
   openMenuButton.setAttribute("aria-hidden", "false");
+  openMenuButton.focus();
+}
 
-  if (shouldFocus) {
-    openMenuButton.focus();
-  }
-
-  if (mediaQuery.matches && !navbar.classList.contains("active")) {
-    disableNavbar();
-  }
+function closeNavbar() {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  openMenuButton.setAttribute("aria-expanded", "false");
+  openMenuButton.setAttribute("aria-hidden", "false");
 }
 
 openMenuButton.addEventListener('click', openNavbar);
-closeMenuButton.addEventListener("click", () => closeNavbar(true));
-overlay.addEventListener("click", () => closeNavbar(true));
+closeMenuButton.addEventListener('click', closeNavbarWithFocus);
+overlay.addEventListener('click', closeNavbarWithFocus);
 
 navLinks.forEach(link => {
-  link.addEventListener("click", () => closeNavbar());
+  link.addEventListener('click', closeNavbar);
 });
 
 // SCROLL ON WINDOW
@@ -95,10 +71,6 @@ window.addEventListener("scroll", () => {
     closeNavbar();
   }
 })
-
-// Listener for media query changes
-mediaQuery.addEventListener("change", updateNavbarVisibility);
-updateNavbarVisibility();
 
 // ACTIVE LINK
 function setActiveLink() {
@@ -140,8 +112,8 @@ navLinks.forEach(link => {
 
       // Announce the full content of the section
       setTimeout(() => {
-        liveRegion.textContent = targetSection.textContent;
-      }, 400);
+        liveRegion.innerHTML = targetSection.innerHTML;
+      }, 100);
     }
   });
 });
